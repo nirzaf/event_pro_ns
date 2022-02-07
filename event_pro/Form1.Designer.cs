fa7a1d3b4b47efd3e5e22311fdd134f0381663a2
nirzaf <mfmfazrin1986@gmail.com>
2022-02-07 20:28:22 +0530

    Add project files.

---
+++ event_pro/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace event_pro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(148, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Hi ! Baby!!!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseHover += new System.EventHandler(this.button1_MouseHover);
+            this.button1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button1_MouseUp);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(148, 57);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 38);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Save Changes F5";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(105, 105);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(194, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(105, 131);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(194, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Enter += new System.EventHandler(this.textBox2_Enter);
+            this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(105, 157);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(194, 20);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Enter += new System.EventHandler(this.textBox3_Enter);
+            this.textBox3.Leave += new System.EventHandler(this.textBox3_Leave);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightGray;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Location = new System.Drawing.Point(151, 183);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 37);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "My Events";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.MouseLeave += new System.EventHandler(this.button3_MouseLeave);
+            this.button3.MouseHover += new System.EventHandler(this.button3_MouseHover);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.checkBox1.Location = new System.Drawing.Point(300, 194);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(85, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Auto Search";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            this.checkBox1.Click += new System.EventHandler(this.checkBox1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Cat",
+            "Dog",
+            "Fish",
+            "Donkey",
+            "Horse",
+            "Koala",
+            "Zebra"});
+            this.comboBox1.Location = new System.Drawing.Point(23, 261);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(35, 288);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(93, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Show Selected";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Cat",
+            "Dog",
+            "Fish",
+            "Donkey",
+            "Horse",
+            "Koala",
+            "Zebra"});
+            this.listBox1.Location = new System.Drawing.Point(265, 227);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 108);
+            this.listBox1.TabIndex = 9;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(282, 341);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(93, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Show Selected";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(69, 357);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 418);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ event_pro/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace event_pro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("You Clicked Me!");
+        }
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("You are on Me!!!!");
+        }
+
+        private void button1_MouseUp(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //MessageBox.Show("I'm Loading...");
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("You Clicked Me ...");
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            MessageBox.Show("I'm Resizing ...");
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // MessageBox.Show("You Pressed a key:"+e.KeyCode.ToString());
+            this.Text = e.KeyCode.ToString();
+            //if (e.KeyCode==Keys.Escape)
+            //{
+            //    this.Close();
+            //}
+            //if (e.KeyCode==Keys.F5)
+            //{
+            //    button2_Click(sender, e);
+            //}
+            //if (e.KeyCode==Keys.F4 )
+            //{
+            //    this.BackColor = Color.Green;
+            //}
+            switch (e.KeyCode)
+            {
+                case Keys.Escape:
+                    this.Close();
+                    break;
+
+                case Keys.F5:
+                    button2_Click(sender, e);
+                    break;
+
+                case Keys.F4:
+                    this.BackColor = Color.Green;
+                    break;
+             
+
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Saved!");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            this.Text = this.textBox1.Text;
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            this.textBox1.BackColor = Color.Yellow;
+        }
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            this.textBox2.BackColor = Color.Yellow;
+        }
+
+        private void textBox3_Enter(object sender, EventArgs e)
+        {
+            this.textBox3.BackColor = Color.Yellow;
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            this.textBox1.BackColor = Color.White;
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            this.textBox2.BackColor = Color.White;
+        }
+
+        private void textBox3_Leave(object sender, EventArgs e)
+        {
+            this.textBox3.BackColor = Color.White;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("I clicked!!!!");
+        }
+
+        private void button3_MouseHover(object sender, EventArgs e)
+        {
+            this.button3.BackColor = Color.Yellow;
+        }
+
+        private void button3_MouseLeave(object sender, EventArgs e)
+        {
+            this.button3.BackColor = Color.LightGray;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            //MessageBox.Show(this.checkBox1.Checked.ToString());
+            //////////if (this.checkBox1.Checked == true)
+            //////////{
+            //////////    MessageBox.Show("You Checked!");
+            //////////}
+            ////////////------------------------------
+            //////////if (this.checkBox1.Checked == false)
+            //////////{
+            //////////    MessageBox.Show("You Unchecked Me!");
+            //////////}
+        }
+
+        private void checkBox1_Click(object sender, EventArgs e)
+        {
+            if (this.checkBox1.Checked == true)
+            {
+                MessageBox.Show("You Checked!");
+            }
+            //------------------------------
+            if (this.checkBox1.Checked == false)
+            {
+                MessageBox.Show("You Unchecked Me!");
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //////this.textBox1.Text = this.comboBox1.SelectedIndex.ToString();
+            //////this.textBox2.Text = this.comboBox1.Text;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (this.comboBox1.SelectedIndex==-1)
+            {
+                MessageBox.Show("First Select Your Item!!!!");
+                return;
+            }
+            this.textBox1.Text = this.comboBox1.SelectedIndex.ToString();
+            this.textBox2.Text = this.comboBox1.Text;
+        }
+
+       
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (this.listBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("First Select Your Item!!!!");
+                return;
+            }
+            this.textBox1.Text = this.listBox1.SelectedIndex.ToString();
+            this.textBox2.Text = this.listBox1.Text;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (this.listBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("First Select Your Item!!!!");
+                return;
+            }
+            this.textBox1.Text = this.listBox1.SelectedIndex.ToString();
+            this.textBox2.Text = this.listBox1.Text;
+        }
+
+
+
+        //private void button6_Click(object sender, EventArgs e)
+        //{
+
+        //}
+    }
+}
